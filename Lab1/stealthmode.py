894cd56ef447914569db4b7a082f61dd95dc1366
David Silva <davidsilvaurrutia@gmail.com>
2023-08-31 04:53:09 -0400

    update: Sistema de envio más sigiloso

--- Lab1/stealthmode.py
+++ Lab1/stealthmode.py
@@ -1,48 +1,37 @@
-import socket
-import struct
-# python3 .\stealthmode.py 192.168.1.1 12
+import sys
+import random
+import time
+from scapy.all import IP, ICMP, send
 
-def ping(host, byte):
-    """
-    Envia un ping a un host con el bit menos significativo modificado.
-
-    Args:
-        host: El host al que enviar el ping.
-        byte: El byte a agregar al bit menos significativo.
-    """
-
-    # Crear un socket ICMP.
-    sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
-
-    # Construir el paquete ICMP.
-    icmp_header = struct.pack(">BBHHH", 8, 0, 0, 0, 0)
-    icmp_payload = struct.pack(">B", byte)
-    packet = icmp_header + icmp_payload
-
-    # Enviar el paquete ICMP.
-    print(sock.sendto(packet, (host, 80)))
+def send_modified_ping(ip, char, seq_num, packet_id):
+    payload = bytearray([0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68,
+                         0x69, 0x6a, 0x6b, 0x6c, 0x6d, 0x6e, 0x6f, 0x70,
+                         0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x61,
+                         0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69])  # Custom payload
+    
+    payload[-1] = ord(char)  # Modify the least significant byte with the ASCII value of the character
+    
+    packet = IP(dst=ip, id=packet_id, ttl=64) / ICMP(type=8, code=0, id=packet_id, seq=seq_num) / payload
+    send(packet)
 
 def main():
-    """
-    Programa principal.
-    """
-    import sys
-    
     if len(sys.argv) != 3:
-        print("Uso: python script.py <IP_destino> <payload>")
-        sys.exit(1)
+        print("Usage: python script.py <ip> <string>")
+        return
     
     ip = sys.argv[1]
-
-    # Obtener la cadena de entrada.
     string = sys.argv[2]
-
-    # Separar la cadena por letras.
-    letters = list(string)
-
-    # Enviar un ping a cada letra.
-    for letter in letters:
-        ping(ip, ord(letter))
-
+    
+    packet_id = 1
+    seq_num = 100 + 10 * random.randint(0, 9)  # Starting seq number with some randomness
+    
+    for char in string:
+        send_modified_ping(ip, char, seq_num, packet_id)
+        print(f"Sent modified ping with character: {char}")
+        
+        packet_id += 1
+        seq_num += 1
+        time.sleep(1)  # Add a delay to simulate realistic timing
+        
 if __name__ == "__main__":
     main()
